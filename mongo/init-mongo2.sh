ca4888c71874f781167cbcff709106955b31b25e
Culver Stone <culver.stone57@gmail.com>
2024-03-19 22:08:09 -0700

    I'm an idiot that got the username/password I assigned to my own container

---
+++ mongo/init-mongo2.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+mongoimport --db='805cts' --collection='events'         --file='/tmp/data/events.json'         --jsonArray --username='admin' --password='admin' --authenticationDatabase=admin
+mongoimport --db='805cts' --collection='example_source' --file='/tmp/data/example_source.json' --jsonArray --username='admin' --password='admin' --authenticationDatabase=admin
+mongoimport --db='805cts' --collection='files'          --file='/tmp/data/files.json'          --jsonArray --username='admin' --password='admin' --authenticationDatabase=admin
+mongoimport --db='805cts' --collection='sources'        --file='/tmp/data/sources.json'        --jsonArray --username='admin' --password='admin' --authenticationDatabase=admin
+mongoimport --db='805cts' --collection='tag_keys'       --file='/tmp/data/tag_keys.json'       --jsonArray --username='admin' --password='admin' --authenticationDatabase=admin
+mongoimport --db='805cts' --collection='tags'           --file='/tmp/data/tags.json'           --jsonArray --username='admin' --password='admin' --authenticationDatabase=admin
