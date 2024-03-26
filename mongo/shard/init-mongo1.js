4c52b0ff2cd4c55894882c22168fbe86973fdc5d
Culver Stone <culver.stone57@gmail.com>
2024-03-25 20:10:47 -0700

    Started adding the rest of the shards to the project

---
+++ mongo/shard/init-mongo1.js
@@ -0,0 +1,8 @@
+db = db.getSiblingDB('805cts')
+
+db.createCollection('events')
+db.createCollection('example_source')
+db.createCollection('files')
+db.createCollection('sources')
+db.createCollection('tag_keys')
+db.createCollection('tags')
